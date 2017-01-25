599e6960cfea348efcafba686a861cb9be548daf
John Kitchin <jkitchin@andrew.cmu.edu>
2017-01-25 09:38:30 -0500

    add lecture 3

--- techela/techela/__init__.py
+++ techela/techela/__init__.py
@@ -1,29 +1,19 @@
-# This code can be put in any Python module, it does not require IPython
-# itself to be running already.  It only creates the magics subclass but
-# doesn't instantiate it yet.
-import collections
+"""A flask of techela."""
 import getpass
-import json
 import os
-import shutil
+import json
 import smtplib
 import subprocess
-import sys
 import time
-import urllib.request
+import urllib
+
+
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-from IPython import get_ipython
-from IPython.core.magic import (Magics, magics_class, line_magic)
-from IPython.core.magic_arguments import (argument, magic_arguments,
-                                          parse_argstring)
-
-from IPython.display import display, Javascript
-from IPython.display import display_javascript
-from IPython.core.display import HTML
-
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
 
 COURSE = 's17-06364'
 COURSEDIR = os.path.expanduser('~/{}/'.format(COURSE))
@@ -57,54 +47,78 @@ ASSIGNMENTURL = BASEURL + 'assignments/'
 SOLUTIONURL = BASEURL + 'solutions/'
 
 
-def open_file(filename):
-    """Cross-platform open file function."""
-    if sys.platform == "win32":
-        os.startfile(filename)
-    else:
-        opener = "open" if sys.platform == "darwin" else "xdg-open"
-        subprocess.call([opener, filename])
+@app.route("/")
+def hello():
+    # TODO download this from github.
+
+    with open('s17-06364.json') as f:
+        data = json.loads(f.read())
+
+    # First get lecture status
+    lecture_labels = data['lectures']
+    lecture_paths = ['{}/lectures/{}.ipynb'.format(COURSEDIR, label)
+                     for label in lecture_labels]
+    lecture_status = ['Downloaded' if os.path.exists(path)
+                      else 'Not downloaded'
+                      for path in lecture_paths]
+
+    # Next get assignments.
+    assignment_labels = data['assignments']
+    assignment_paths = ['{}/assignments/{}-{}.ipynb'.format(COURSEDIR,
+                                                            ANDREWID,
+                                                            label)
+                        for label in assignment_labels]
+    assignment_status = ['Downloaded' if os.path.exists(path)
+                         else 'Not downloaded'
+                         for path in assignment_paths]
+
+    duedates = []
+    turned_in = []
+    for path in assignment_paths:
+        with open(path) as f:
+            d = json.loads(f.read())
+            duedates.append(d['metadata']['org']['DUEDATE'])
+            turned_in.append(d['metadata'].get('TURNED-IN', None))
+
+    return render_template('hello.html',
+                           COURSEDIR=COURSEDIR,
+                           ANDREWID=ANDREWID,
+                           NAME=NAME,
+                           lectures=zip(lecture_labels, lecture_status),
+                           assignments=zip(assignment_labels,
+                                           assignment_status,
+                                           duedates,
+                                           turned_in))
 
 
-# * User magic
-@magics_class
-class Techela(Magics):
-    """IPython Magics for students.
-    It is a command like this:
-    > techela open label
-    """
+@app.route("/lecture/<label>")
+def open_lecture(label):
+    fname = '{}/lectures/{}.ipynb'.format(COURSEDIR, label)
+    if not os.path.exists(fname):
+        urllib.request.urlretrieve(LECTUREURL
+                                   + '{}.ipynb'.format(label), fname)
+        # We need to check for images. Boo...
+        # They look like this in the cells: ![img](./images/control-volume.png)
 
-    @magic_arguments()
-    @argument('-l', '--lecture', type=str, help='Open a lecture.')
-    @argument('-o', '--open', type=str, help='Open an assignment.')
-    @argument('-s', '--solution', type=str, help='Open a solution.')
-    @argument('-t', '--turn-in', type=str, help='Turn in an assignment.')
-    @line_magic
-    def techela(self, line):
-        """IPython magic function to get and turn in assignments."""
+    # Now open the notebook.
+    cmd = ["jupyter", "notebook", fname]
+    subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE,
+                     stdin=subprocess.PIPE)
 
-        args = parse_argstring(self.techela, line)
+    return redirect(url_for('hello'))
 
-        if args.open:
-            self.open(args.open)
-        elif args.lecture:
-            self.lecture(args.lecture)
-        elif args.solution:
-            self.solution(args.solution)
-        elif args.turn_in:
-            self.turn_in(args.turn_in)
 
-    def open(self, label):
-        """Open label.ipynb, and download if needed."""
-        print('Opening {}'.format(label))
-        fname = '{}/assignments/{}-{}.ipynb'.format(COURSEDIR,
-                                                    ANDREWID,
-                                                    label)
-        if not os.path.exists(fname):
-            print('Downloading {}.'.format(ASSIGNMENTURL + fname))
-            urllib.request.urlretrieve('{}/{}.ipynb'.format(ASSIGNMENTURL,
-                                                            label),
-                                       fname)
+@app.route("/assignment/<label>")
+def open_assignment(label):
+    print('Opening {}'.format(label))
+    fname = '{}/assignments/{}-{}.ipynb'.format(COURSEDIR,
+                                                ANDREWID,
+                                                label)
+    if not os.path.exists(fname):
+        urllib.request.urlretrieve('{}/{}.ipynb'.format(ASSIGNMENTURL,
+                                                        label),
+                                   fname)
 
         # Insert their full name at the top
         with open(fname) as f:
@@ -125,256 +139,74 @@ class Techela(Magics):
         with open(fname, 'w') as f:
             f.write(json.dumps(j))
 
-        # Now open the notebook.
-        subprocess.Popen(['jupyter', 'notebook', fname])
+    # Now open the notebook.
+    # os.system('jupyter notebook {}'.format(fname))
+    cmd = ["jupyter", "notebook", fname]
+    subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE,
+                     stdin=subprocess.PIPE)
 
-    def solution(self, label):
-        """Open label.ipynb, and download if needed."""
-        fname = '{}/solutions/{}.ipynb'.format(COURSEDIR, label)
-        if not os.path.exists(fname):
-            print('Downloading {}.'.format(SOLUTIONURL + fname))
-            urllib.request.urlretrieve(SOLUTIONURL
-                                       + '{}.ipynb'.format(label), fname)
-
-        # Now open the notebook.
-        subprocess.Popen(['jupyter', 'notebook', fname])
-
-    def lecture(self, label):
-        """Open label.ipynb, and download if needed."""
-        fname = '{}/lectures/{}.ipynb'.format(COURSEDIR, label)
-        if not os.path.exists(fname):
-            print('Downloading {}.'.format(LECTUREURL + fname))
-            urllib.request.urlretrieve(LECTUREURL
-                                       + '{}.ipynb'.format(label), fname)
-
-        # Now open the notebook.
-        subprocess.Popen(['jupyter', 'notebook', fname])
-
-# ** Turn it in
-    def turn_in(self, label):
-        """Turn in LABEL by email."""
-        # Create the container (outer) email message.
-        msg = MIMEMultipart()
-        subject = '[{}] - Turning in {}'
-        msg['Subject'] = subject.format(COURSE, label)
-        msg['From'] = '{}@andrew.cmu.edu'.format(ANDREWID)
-        msg['To'] = BOX_EMAIL
-        msg['Cc'] = '{}@andrew.cmu.edu'.format(ANDREWID)
-
-        ctype = 'application/octet-stream'
-        maintype, subtype = ctype.split('/', 1)
-
-        fname = '{}/assignments/{}-{}.ipynb'.format(COURSEDIR,
-                                                    ANDREWID,
-                                                    label)
-        if not os.path.exists(fname):
-            raise Exception('{} not found.'.format(fname))
-
-        # Save some turn in data.
-        with open(fname) as f:
-            j = json.loads(f.read())
-
-        j['metadata']['TURNED-IN'] = {}
-        j['metadata']['TURNED-IN']['timestamp'] = time.asctime()
-
-        with open(fname, 'w') as f:
-            f.write(json.dumps(j))
-
-        with open(fname, 'rb') as fp:
-            attachment = MIMEBase(maintype, subtype)
-            attachment.set_payload(fp.read())
-            # Encode the payload using Base64
-            encoders.encode_base64(attachment)
-            # Set the filename parameter
-            aname = '{}-{}.ipynb'.format(ANDREWID, label)
-            attachment.add_header('Content-Disposition', 'attachment',
-                                  filename=aname)
-            msg.attach(attachment)
-
-        with smtplib.SMTP_SSL('smtp.andrew.cmu.edu', port=465) as s:
-            s.login(ANDREWID, getpass.getpass('Andrew password: '))
-            s.send_message(msg)
-            s.quit()
-
-        print('{} submitted.'.format(fname))
+    return redirect(url_for('hello'))
 
 
-# * Admin magic
-def get_andrewids():
-    """Return a list of andrew ids."""
-    import csv
-
-    with open('course/roster.csv') as f:
-        reader = csv.reader(f)
-        rows = [row for row in reader]
-    return [x[8] for x in rows]
+@app.route("/submit/<label>")
+def authenticate(label):
+    "Get the andrew password."
+    return render_template("email.html", label=label)
 
 
+@app.route("/submit_post", methods=['POST'])
+def submit_post():
+    """Turn in LABEL by email."""
 
-multipliers = collections.OrderedDict([('A++', 1),
-                                       ('A+', 0.95),
-                                       ('A', 0.9),
-                                       ('A-', 0.85),
-                                       ('A/B', 0.8),
-                                       ('B+', 0.75),
-                                       ('B', 0.7),
-                                       ('B-', 0.65),
-                                       ('B/C', 0.6),
-                                       ('C+', 0.55),
-                                       ('C', 0.5),
-                                       ('C-', 0.45),
-                                       ('C/D', 0.4),
-                                       ('D+', 0.35),
-                                       ('D', 0.3),
-                                       ('D-', 0.25),
-                                       ('D/R', 0.2),
-                                       ('R+', 0.15),
-                                       ('R', 0.1),
-                                       ('R-', 0.05),
-                                       ('R--', 0.0)])
+    password = request.form['password']
+    label = request.form['label']
 
-rubrics = {'default': [['technical', 0.7],
-                       ['presentation', 0.3]]}
+    # Create the container (outer) email message.
+    msg = MIMEMultipart()
+    subject = '[{}] - Turning in {}'
+    msg['Subject'] = subject.format(COURSE, label)
+    msg['From'] = '{}@andrew.cmu.edu'.format(ANDREWID)
+    msg['To'] = BOX_EMAIL
+    msg['Cc'] = '{}@andrew.cmu.edu'.format(ANDREWID)
 
+    ctype = 'application/octet-stream'
+    maintype, subtype = ctype.split('/', 1)
 
-@magics_class
-class TechelaAdmin(Magics):
-    """IPython Magics."""
-    @magic_arguments()
-    @argument('-c', '--collect', type=str, help='Collect an assignment')
-    @argument('-s', '--summarize', type=str, help='Grade an assignment')
-    @argument('-r', '--return-assignment', type=str,
-              help='Return an assignment')
-    @line_magic
-    def ta(self, line):
-        """IPython magic function to get and turn in assignments."""
-        args = parse_argstring(self.ta, line)
-
-        if args.collect:
-            print('Collecting {}'.format(args.collect))
-            self.collect(args.collect)
-
-        elif args.return_assignment:
-            print('Returning {}'.format(args.return_assignment))
-            self.return_assignment(args.return_assignment)
-
-    def collect(self, label):
-        """Collect LABEL assignment.
-
-        Collect means copy all the files matching {andrew-id}-label.ipynb
-        from the submission directory to the label directory.
-
-        You should run this in the root directory where the submissions folder
-        is. It will create the label directory.
-
-        """
-        adir = 'assignments/{}/'.format(label)
-        if not os.path.isdir(adir):
-            os.makedirs(adir)
-
-        for andrewid in get_andrewids():
-            fname = 'submissions/{}-{}.ipynb'.format(andrewid, label)
-
-            if os.path.exists(fname):
-                shutil.move(fname, adir)
-                print('Moved {} to {}'.format(fname, adir))
-            else:
-                print('No submission for {}'.format(andrewid))
-
-    def return_assignment(self, label):
-        """Return the graded files by email.
-
-        This does not keep track of what has been sent, so it will send them
-        all every time!
-
-        """
-
-        password = getpass.getpass('Andrew password: ')
-
-        for andrewid in get_andrewids():
-            # I am not sure we have to reauthenticate each time. But an earlier
-            # version had s.quit outside the context manager and it caused an
-            # error.
-            with smtplib.SMTP_SSL('smtp.andrew.cmu.edu', port=465) as s:
-                s.login(self.data['andrewid'], password)
-                msg = MIMEMultipart()
-                subject = '[{}] - {} has been graded'
-                msg['Subject'] = subject.format(self.data['course'],
+    fname = '{}/assignments/{}-{}.ipynb'.format(COURSEDIR,
+                                                ANDREWID,
                                                 label)
-                msg['From'] = '{}@andrew.cmu.edu'.format(self.data['andrewid'])
-                msg['To'] = '{}@andrew.cmu.edu'.format(andrewid)
+    if not os.path.exists(fname):
+        raise Exception('{} not found.'.format(fname))
 
-                ctype = 'application/octet-stream'
-                maintype, subtype = ctype.split('/', 1)
+    # Save some turn in data.
+    with open(fname) as f:
+        j = json.loads(f.read())
 
-                fname = 'assignments/{}/{}-{}.ipynb'.format(label, andrewid, label)
+    j['metadata']['TURNED-IN'] = {}
+    j['metadata']['TURNED-IN']['timestamp'] = time.asctime()
 
-                if not os.path.exists(fname):
-                    print('{} not found. Continuing.'.format(fname))
-                    continue
+    with open(fname, 'w') as f:
+        f.write(json.dumps(j))
 
-                with open(fname, 'rb') as fp:
-                    attachment = MIMEBase(maintype, subtype)
-                    attachment.set_payload(fp.read())
-                    # Encode the payload using Base64
-                    encoders.encode_base64(attachment)
-                    # Set the filename parameter
-                    attachment.add_header('Content-Disposition', 'attachment',
-                                          filename=pdf)
-                    msg.attach(attachment)
+    with open(fname, 'rb') as fp:
+        attachment = MIMEBase(maintype, subtype)
+        attachment.set_payload(fp.read())
+        # Encode the payload using Base64
+        encoders.encode_base64(attachment)
+        # Set the filename parameter
+        aname = '{}-{}.ipynb'.format(ANDREWID, label)
+        attachment.add_header('Content-Disposition', 'attachment',
+                              filename=aname)
+        msg.attach(attachment)
 
-                s.send_message(msg)
-                s.quit()
-                print('{} returned.'.format(fname))
-                # Pause so we don't get kicked out
-                time.sleep(0.5)
+    with smtplib.SMTP_SSL('smtp.andrew.cmu.edu', port=465) as s:
+        s.login(ANDREWID, password)
+        s.send_message(msg)
+        s.quit()
 
-        print('Done returning {}.'.format(label))
-
-    # @line_magic
-    # def grade(self, line):
-    #     """A widget to add a grade based on the rubric in the notebook."""
-    #     # First we have to get the rubric.
-    #     # It is stored by name in metadata.org.RUBRIC
-
-        
-        
-    #     # tech = input('technical: ').upper()
-    #     # pres = input('presentation: ').upper()
-
-    #     # if not (tech in multipliers and pres in multipliers):
-    #     #     raise Exception('Invalid grade entered.')
-
-    #     # grade = 0.7 * multipliers[tech] + 0.3 * multipliers[pres]
-
-    #     # letter_grade = [(k, v) for k, v in multipliers.items() if v < grade][0][0]
-
-    #     # s1 = 'IPython.notebook.metadata.grade.technical = "{}";'.format(tech)
-    #     # s2 = 'IPython.notebook.metadata.grade.presentation = "{}";'.format(pres)
-    #     # s3 = 'IPython.notebook.metadata.grade.overall = {};'.format(grade)
-
-    #     # display_javascript('IPython.notebook.metadata.grade = {};', raw=True)
-    #     # display_javascript(s1, raw=True)
-    #     # display_javascript(s2, raw=True)
-    #     # display_javascript(s3, raw=True)
-    #     # display(Javascript('IPython.notebook.save_checkpoint();'))
-
-    #     # return 'Overall grade: {0:1.3f} ({1})'.format(grade, letter_grade)
-
-    # @line_magic
-    # def comment(self, line):
-    #     s = '<font color="red">{0}: {1}</font>'
-    #     comment = input('Comment: ')
-    #     return HTML(s.format(ANDREWID, comment))        
+    return redirect(url_for('hello'))
 
 
-# * Register the extensions and magics
-
-# In order to actually use these magics, you must register them with a
-# running IPython.  This code must be placed in a file that is loaded once
-# IPython is up and running:
-ip = get_ipython()
-# You can register the class itself without instantiating it.  IPython will
-# call the default constructor on it.
-ip.register_magics(Techela)
-ip.register_magics(TechelaAdmin)
+if __name__ == "__main__":
+    app.run()
